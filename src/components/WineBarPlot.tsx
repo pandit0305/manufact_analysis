0726edefc721ae6e0414ef213a723f3fc395c72d
Gangadhar-CCS <pandit.gangadhar@codectrlsolutions.com>
2023-02-23 17:30:55 +0530

    bar chart is updated

--- src/components/WineBarPlot.tsx
+++ src/components/WineBarPlot.tsx
@@ -2,36 +2,40 @@ import Data from '../Wine-Data.json';
 import ReactEcharts from "echarts-for-react";
 
 function WineBarPlot() {
-    const new_data: number[] = [];
+    const alcohol_data: number[] = [];
+    const malic_acid_data: number[]=[];
       Data.map((ele)=>{
-        const el = Number(ele['Alcohol'])
-        return new_data.push(el);
+        alcohol_data.push(Number(ele['Alcohol']));
+        malic_acid_data.push(Number(ele['Malic Acid']));
         
       })
+      const sum_malic_data = malic_acid_data.reduce((i, cb)=>{
+        return cb+i
+      })
+      const average_malic_data = Math.floor(sum_malic_data/malic_acid_data.length);
+      console.log(average_malic_data);
+      
    const  option = {
       legend: {},
       xAxis: [
         {
           type: 'category',
           name:"Alcohol",
-          min:0,
-          max:Math.max(...new_data)
+          min:1,
+          max:Math.max(...alcohol_data)
         }
       ],
       yAxis: [
         {
           type: 'value',
           name: 'Malic Acid',
-          min:0,
-          max:Math.max(...new_data)
         }
       ],
       series: [
         {
           name: 'Wine Data',
           type: 'bar',
-          yAxisIndex: 0,
-          data: new_data
+          data: malic_acid_data
         }
       ]
     };
